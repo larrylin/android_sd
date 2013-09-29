e779273cb90adede17e4ead4b3d9193a43366b16
larrylin <u39448@gmail.com>
2013-09-29 10:22:42 +0800

    测试项目

---
+++ A_Layout_AbsoluteLayout/gen/com/example/a_layout_absolutelayout/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.a_layout_absolutelayout;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
